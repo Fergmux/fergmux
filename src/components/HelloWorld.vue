ed7676d1a9d1fdc7bb351e00f7b2700ed72e6ee4
Fergus Mullineux <fergmux@gmail.com>
2022-02-04 14:21:57 +0000

    feat: add vite and tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,143 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="about">
+    <h1 class="text-xl mb-5">This is the chart page</h1>
+    <button :disabled="!state.indexes.length" @click="drillup" class="mb-5 px-6 py-2 rounded bg-emerald-400 hover:bg-emerald-500 text-emerald-100 disabled:bg-slate-300 disabled:text-slate-600">Go back</button>
+
+    <div style="width: 100%; display: flex; justify-content: center">
+      <div
+        class="chart-container"
+        style="position: relative; height: 100vh; width: 100vh"
+      >
+        <canvas ref="chartRef"></canvas>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+import { ref, reactive, onMounted } from "vue";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+const gameData = [
+  {
+    key: "Microsoft",
+    value: 2358.99,
+    children: [
+      {
+        key: "Xbox Game Studios",
+        value: 2221,
+        children: [
+          { key: "Rare", value: 100, children: [] },
+          { key: "343 Industries", value: 100, children: [] },
+          { key: "The Coalition", value: 100, children: [] },
+          { key: "Mojang", value: 100, children: [] },
+          { key: "Ninja Theory", value: 100, children: [] },
+          { key: "Playground Games", value: 100, children: [] },
+          { key: "Undead labs", value: 100, children: [] },
+          { key: "Obsidian Entertainment", value: 100, children: [] },
+          { key: "Double Fine", value: 100, children: [] },
+        ],
+      },
+      { key: "Activision Blizzard", value: 61.49, children: [] },
+      { key: "Zenimax Media", value: 61.49, children: [] },
+    ],
+  },
+  {
+    key: "Other",
+    value: 963.8,
+    children: [
+      { key: "Tencent Games", value: 593.66, children: [] },
+      { key: "No Parent", value: 280.52, children: [] },
+      { key: "EA", value: 38.8, children: [] },
+      { key: "Take-Two Interactive", value: 18.8, children: [] },
+      {
+        key: "Warner Bros. Interactive Entertainment",
+        value: 22.18,
+        children: [],
+      },
+      { key: "Embracer Group", value: 9.84, children: [] },
+    ],
+  },
+  {
+    key: "Sony",
+    value: 135.36,
+    children: [],
+  },
+  { key: "Nintendo", value: 55, children: [] },
+];
+
+const getLabels = (data) => data.map((item) => item.key);
+const getData = (data) => data.map((item) => item.value);
+
+const data = {
+  labels: getLabels(gameData),
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: getData(gameData),
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const About = {
+  setup() {
+    const chartRef = ref(null);
+    const state = reactive({ indexes: [] });
+    let chart;
+
+    const refreshChart = () => {
+      let currentData = gameData;
+      state.indexes.forEach(
+        (index) => (currentData = currentData[index].children)
+      );
+      chart.data.labels = getLabels(currentData);
+      chart.data.datasets[0].data = getData(currentData);
+      chart.update();
+    };
+
+    const drilldown = (event) => {
+      const index = chart.getElementsAtEventForMode(
+        event,
+        "nearest",
+        { intersect: true },
+        true
+      )[0].index;
+
+      state.indexes.push(index);
+      refreshChart();
+    };
+
+    const drillup = () => {
+      state.indexes.pop();
+      refreshChart();
+    };
+
+    Chart.overrides.doughnut.plugins.tooltip = {
+      callbacks: {
+        label: (tooltipItem) => `$${tooltipItem.parsed}B`,
+      },
+    };
+
+    onMounted(() => {
+      chart = new Chart(chartRef.value, {
+        type: "doughnut",
+        data,
+        options: {
+          onClick: drilldown,
+        },
+      });
+    });
+
+    return { chartRef, chart, drillup, state };
   },
 };
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default About;
+</script>
