0775a32fecb74f7e20866a79260bc0d4570c9d6f
Fergus Mullineux <fergmux@gmail.com>
2022-03-05 23:59:20 +0000

    fix: font import url

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,6 @@ export default {
 
 <style>
 .app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   font-family: 'Inter', Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
